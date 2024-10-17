87c5e383fa467d23ce7c38c74c47e82779d4c26a
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-10-18 01:42:53 +0300

    General Mapping Class

---
+++ Foody.PresentationLayer/Mappings/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Foody.DTO.DTOs.AboutDTOs;
+using Foody.DTO.DTOs.SliderDTOs;
+using Foody.EntityLayer.Concrete;
+
+namespace Foody.PresentationLayer.Mappings
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<ResultAboutDTO, About>().ReverseMap();
+            CreateMap<CreateAboutDTO, About>().ReverseMap();
+            CreateMap<GetByIdAboutDTO, About>().ReverseMap();
+            CreateMap<UpdateAboutDTO, About>().ReverseMap();
+
+            CreateMap<ResultSliderDTO, Slider>().ReverseMap();
+            CreateMap<CreateSliderDTO, Slider>().ReverseMap();
+            CreateMap<GetByIdSliderDTO, Slider>().ReverseMap();
+            CreateMap<UpdateSliderDTO, Slider>().ReverseMap();
+        }
+    }
+}
